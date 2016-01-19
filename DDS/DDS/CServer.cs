7afd98d54297e439191fd1ca0727153f353fec5f
biggreyhairboy <biggreyhairboy@gmail.com>
2016-01-19 16:14:05 +0800

    common中间不必要的组建暂时去掉了， 只保留了目前使用的socket的依赖类（好像order和position的也有）；现在image  open(update ,image, both) 模式都可以用了，接下来实现field的update；另外还需要写一个批处理来发送静态的消息和更新image

--- DDS/DDS/CServer.cs
+++ DDS/DDS/CServer.cs
@@ -6,9 +6,10 @@ using System.Threading.Tasks;
 using System.Collections;
 using System.Configuration;
 using System.Collections.Specialized;
-
+using System.Globalization;
 using OMS.lib;
 using OMS.common.Sockets;
+using OMS.utils;
 
 namespace OMS
 {
@@ -19,14 +20,23 @@ namespace OMS
         public string currentMsg;
         public List<string> openall;
 
-        public Dictionary<string, string> TSubTable;
-        public string RcvMessage;
+        //store contribute images
+        public List<string> SymbolsList;
+        public Dictionary<string, string> SymbolImageDictionary;
 
+        //store handles mapping relationships
+        public Dictionary<string, List<string>> SymbolHandlesDictionary;
+        public Dictionary<string, TSocketClient> HandleClientDictionary;
 
         //public Dictionary<string, oms>
 
         public DDSServer(string ip, int port):base(ip, port)
         {
+            SymbolsList = new List<string>();
+            SymbolImageDictionary = new Dictionary<string, string>();
+            SymbolHandlesDictionary = new Dictionary<string, List<string>>();
+            HandleClientDictionary = new Dictionary<string, TSocketClient>();
+
             DDSInitializer();
         }
 
@@ -51,6 +61,10 @@ namespace OMS
             Console.WriteLine(">DDS|" + e.Message);
             omsLog.log.Info(">DDS|" + e.Message);
             string msg = e.Message;
+            if (!msg.Contains('|'))
+            {
+                return;                
+            }
             string command =  msg.Substring(0, msg.IndexOf('|')).ToLower();
             //代替了omsclients中的processmessage的功能
             if(msg.IndexOf('|') == -1 )
@@ -58,17 +72,51 @@ namespace OMS
                 omsLog.log.Error("error command: " + msg);
                 return;
             }
-            if(command == "open" ||command == "openall" ||command == "close" ||command == "list" ||command == "session" ||command == "dump" ||command == "dumpfile")
+            
+
+            if (command == "image" || command == "udpate" || command == "listsetup" || command == "reset")
+            {
+                string currentsymbol = getSymbol(msg, DDSCommandType.Contribute);
+                //message from contributor
+                //todo:先有contributor 再有subscriber
+                switch (command)
+                {
+                    case "image":
+                        if (!SymbolImageDictionary.ContainsKey(currentsymbol))
+                        {
+                            asImageNew(e.Socket, e.Message, currentsymbol);
+                        }
+                        else
+                        {
+                            SymbolImageDictionary[currentsymbol] = e.Message;
+                            asImage(e.Socket, e.Message, currentsymbol);
+                        }
+
+                        break;
+                    case "update":
+                        asUpdate(e.Socket, e.Message);
+                        break;
+                    case "listsetup":
+                        asListsetup(e.Socket, e.Message);
+                        break;
+                    case "reset":
+                        asReset(e.Socket, e.Message);
+                        break;
+                }
+            }else if (command == "open" || command == "openall" || command == "close" || command == "list" ||
+                     command == "session" || command == "dump" || command == "dumpfile")
             {
                 //message from a subscriber
                 //todo: 先实现openall|
                 //e.Socket.SendMessage("on message i call you back");
                 //非常关键的一点 根据remoteaddress来从clientlist中帅选回复的客户端。
                 //MessageAction(e.Socket, e.Message);
-                switch(command)
+                string currenthandle = getHandle(msg);
+                DDSCommandMode currentmode = getMode(msg);
+                switch (command)
                 {
                     case "open":
-                        asOpen(e.Socket, e.Message);
+                        asOpen(e.Socket, e.Message, currenthandle, currentmode);
                         break;
                     case "openall":
                         asOpenAll(e.Socket, e.Message);
@@ -88,43 +136,96 @@ namespace OMS
                     case "dumpfile":
                         asDumpFile(e.Socket, e.Message);
                         break;
-                    default:
-                        break;
                 }
 
                 clientList[e.Socket.RemoteAddress].SendMessage("can i ?");
             }
-            else if(command == "image" && command == "udpate" && command == "listsetup" && command == "reset")
-            {
-                //message from contributor
-                //todo:先有contributor 再有subscriber
-                switch (command)
-                {
-                    case "iamge":
-                        asImage(e.Socket, e.Message);
-                        break;
-                    case "update":
-                        asUpdate(e.Socket, e.Message);
-                        break;
-                    case "listsetup":
-                        asListsetup(e.Socket, e.Message);
-                        break;
-                    case "reset":
-                        asReset(e.Socket, e.Message);
-                        break;
-                    default:
-                        break;
-                }
-                //this.currentClient = clientList[((TSocketClient)sender).];
-                
-            }
+            //this.currentClient = clientList[((TSocketClient)sender).];                           
            // DDSServer.SendClientMsg("writeback: i have received you request!")
             //clientList.Add(e.
             
             //todo: 至少需要保持两个dict 1. 客户端根据名称来索引  2.信息根据各种类型特征来索引
         }
 
-        
+        private DDSCommandMode getMode(string msg)
+        {
+            string tmp = msg.TrimEnd('|');
+            string mode = tmp.Substring(tmp.LastIndexOf('|') + 1, tmp.Length - tmp.LastIndexOf('|') - 1);
+            DDSCommandMode ddsCommandMode = DDSCommandMode.image;
+            switch (mode)
+            {
+                case "image":
+                    ddsCommandMode = DDSCommandMode.image;
+                    break;
+                case "upate":
+                    ddsCommandMode = DDSCommandMode.update;
+                    break;
+                case "both":
+                    ddsCommandMode = DDSCommandMode.both;
+                    break;
+            }
+            return ddsCommandMode;
+        }
+
+        /// <summary>
+        /// 根据命令的不同类型获取symbol的值
+        /// </summary>
+        /// <param name="tmpmsg">从socket获取的一条命令</param>
+        /// <param name="commandType">命令的类型</param>
+        /// <returns></returns>
+        private string getSymbol(string tmpmsg, DDSCommandType commandType)
+        {            
+            if (commandType == DDSCommandType.Subscribe)
+            {
+                int commandLength = tmpmsg.IndexOf('|') + 1;
+                string handlestr = tmpmsg.Substring(commandLength, tmpmsg.Length - commandLength);
+                if (handlestr.Trim() != "")
+                {
+                    string handle = handlestr.Substring(0, handlestr.IndexOf('|'));
+                    string symbolstring = handlestr.Substring(handle.Length + 1, handlestr.Length - handle.Length -1);
+                    string symbol = symbolstring.Substring(0, symbolstring.IndexOf('|'));
+                    return symbol;
+                }
+                else
+                {
+                    omsLog.log.Error(tmpmsg);
+                    return "";
+                }
+            }
+            else
+            {
+                int commandLength = tmpmsg.IndexOf('|') + 1;
+                string symbolstring = tmpmsg.Substring(commandLength, tmpmsg.Length - commandLength);
+                if (symbolstring.Trim() != "")
+                {
+                    string symbol = symbolstring.Substring(0, symbolstring.IndexOf('|'));
+                    return symbol;
+                }
+                else
+                {
+                    omsLog.log.Error(tmpmsg);
+                    return "";
+                }
+            }
+          
+        }
+
+        private string getHandle(string msg)
+        {
+            int commandLength = msg.IndexOf('|') + 1;
+            string handlestr = msg.Substring(commandLength, msg.Length - commandLength);
+            if (handlestr.Trim() != "")
+            {
+                string handle = handlestr.Substring(0, handlestr.IndexOf('|'));
+                return handle;
+            }
+            else
+            {
+                omsLog.log.Error(msg);
+                return "";
+            }
+        }
+
         private void DDSServer_OnClientConnect(object sender, SocketClientConnectEventArgs e)
         {
             omsLog.log.Debug("client connectted!");
@@ -148,10 +249,40 @@ namespace OMS
             return true;
         }
 
-        public bool asOpen(TSocketClient socketclient, string msg)
+        public bool asOpen(TSocketClient socketclient, string msg, string handle, DDSCommandMode mode)
         {
-            return true;
+            string symbol = getSymbol(msg, DDSCommandType.Subscribe);            
+            if (SymbolImageDictionary.ContainsKey(symbol))
+            {
+                switch (mode)
+                {
+                    case DDSCommandMode.image:
+                        clientList[socketclient.RemoteAddress].SendMessage(SymbolImageDictionary[symbol]);
+                        break;
+                    case DDSCommandMode.update:
+                        SymbolHandlesDictionary.Add(symbol, new List<string>(){handle});
+                        HandleClientDictionary.Add(handle, socketclient);
+                        break;
+                    case DDSCommandMode.both:
+                        clientList[socketclient.RemoteAddress].SendMessage(SymbolImageDictionary[symbol]);
+                        SymbolHandlesDictionary.Add(symbol, new List<string>() { handle });
+                        HandleClientDictionary.Add(handle, socketclient);
+                        break;
+                    default:
+                        return false;
+                        break;
+                }
+                return true;
+            }
+            else
+            {
+                socketclient.SendMessage("image not available!");
+                return false;
+            }
+
         }
+
+
         public bool asList(TSocketClient socketclient, string msg)
         {
             return true;
@@ -166,10 +297,7 @@ namespace OMS
         {
             return true;
         }
-        public bool asImage(TSocketClient socketclient, string msg)
-        {
-            return true;
-        }
+
         public bool asUpdate(TSocketClient socketclient, string msg)
         {
             return true;
@@ -196,8 +324,27 @@ namespace OMS
         {
             return true;
         }
-        public bool asImageNew(TSocketClient socketclient, string msg)
+
+        public bool asImage(TSocketClient socketclient, string msg, string symbol)
         {
+            //string symb = msg.Substring(msg.IndexOf('|')+ 1, msg.Length - 1);
+            if (SymbolHandlesDictionary.ContainsKey(symbol))
+            {
+                foreach (KeyValuePair<string, TSocketClient> kvp in HandleClientDictionary)
+                {
+                    kvp.Value.SendMessage(msg);
+                    //或者用clientlists来发送推送信息
+                    clientList[kvp.Value.RemoteAddress].SendMessage(msg);
+                }
+            }
+            return true;
+        }
+
+        public bool asImageNew(TSocketClient socketclient, string msg, string symbol)
+        {
+            //todo：如果在image存在之前就已经有订阅了，是否需要推送, 暂时觉得不应该推送
+            SymbolsList.Add(symbol);
+            SymbolImageDictionary.Add(symbol, msg);
             return true;
         }
         public bool asUpdateNew(TSocketClient socketclient, string msg)
@@ -240,4 +387,5 @@ namespace OMS
 
         //}
     }
+
 }
---
+++ DDS/DDS/utils/omsUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMS.utils
+{
+    enum DDSCommandType
+    {
+        Subscribe = 0,
+        Contribute = 1
+    }
+
+    enum DDSCommandMode
+    {
+        image = 0,
+        update = 1,
+        both = 2
+    }
+}
